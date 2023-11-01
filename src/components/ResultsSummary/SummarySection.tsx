64195f485c6943c6880c9a63e4d24ff97aac5296
Jason Luo <yulaie1012@gmail.com>
2023-11-02 03:02:17 +0800

    fix

--- src/components/ResultsSummary/SummarySection.tsx
+++ src/components/ResultsSummary/SummarySection.tsx
@@ -10,7 +10,7 @@ function SummarySection() {
           }}
         >
           <div className="flex space-x-[12px]">
-            <img className="h-[20px]" src="/src/assets/images/icon-reaction.svg" alt="Reaction" />
+            <img src="/src/assets/images/icon-reaction.svg" alt="Reaction" />
             <h3 className="text-[16px] font-medium text-[#F55] desktop:text-[20px]">Reaction</h3>
           </div>
           <p className="text-[16px] font-bold text-[#303B59] desktop:text-[18px]">
@@ -24,7 +24,7 @@ function SummarySection() {
           }}
         >
           <div className="flex space-x-[12px]">
-            <img className="h-[20px]" src="/src/assets/images/icon-memory.svg" alt="Memory" />
+            <img src="/src/assets/images/icon-memory.svg" alt="Memory" />
             <h3 className="text-[16px] font-medium text-[#FFB21E] desktop:text-[18px]">Memory</h3>
           </div>
           <div className="flex space-x-[8px]"></div>
@@ -39,7 +39,7 @@ function SummarySection() {
           }}
         >
           <div className="flex space-x-[12px]">
-            <img className="h-[20px]" src="/src/assets/images/icon-verbal.svg" alt="Verbal" />
+            <img src="/src/assets/images/icon-verbal.svg" alt="Verbal" />
             <h3 className="text-[16px] font-medium text-[#00BB8F] desktop:text-[18px]">Verbal</h3>
           </div>
           <div className="flex space-x-[8px]"></div>
@@ -54,7 +54,7 @@ function SummarySection() {
           }}
         >
           <div className="flex space-x-[12px]">
-            <img className="h-[20px]" src="/src/assets/images/icon-visual.svg" alt="Visual" />
+            <img src="/src/assets/images/icon-visual.svg" alt="Visual" />
             <h3 className="text-[16px] font-medium text-[#1125D6] desktop:text-[18px]">Visual</h3>
           </div>
           <div className="flex space-x-[8px]"></div>
