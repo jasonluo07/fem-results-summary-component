12ef4a1ada7da429ef1d0c7609201d6d1675f2ce
Jason Luo <yulaie1012@gmail.com>
2023-11-02 00:13:07 +0800

    feat: add SummarySection component

--- src/components/SummarySection.tsx
+++ src/components/SummarySection.tsx
@@ -1,5 +1,15 @@
 function SummarySection() {
-  return <></>;
+  return <section>
+    <h2>Summary</h2>
+    <ul>
+      <li>
+        <div>
+          <img src="/src/assets/images/icon-reaction.svg" alt="Reaction" />
+          Reaction
+        </div>
+      </li>
+    </ul>
+  </section>;
 }
 
 export default SummarySection;
