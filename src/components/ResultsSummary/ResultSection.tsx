25ba5497e0d05d012094ccf5926fc2c0153bd1a8
Jason Luo <yulaie1012@gmail.com>
2023-11-02 02:24:52 +0800

    refactor: optimize code

--- src/components/ResultsSummary/ResultSection.tsx
+++ src/components/ResultsSummary/ResultSection.tsx
@@ -1,17 +1,17 @@
 function ResultSection() {
   return (
-    <section className="desktop:rounded-[32px] desktop:w-1/2 flex flex-col items-center rounded-b-[32px] bg-gradient-to-b from-[#75F] to-[#2F2CE9] pb-10 pt-6 shadow-[0_30px_60px_0px_rgba(61,108,236,0.15)]">
-      <h1 className="desktop:text-[24px] desktop:mb-9 mb-6 text-[18px] font-bold leading-normal text-[#CAC9FF]">
+    <section className="desktop:rounded-[32px] desktop:w-1/2 flex flex-col items-center rounded-b-[32px] bg-gradient-to-b from-[#75F] to-[#2F2CE9] pb-[40px] pt-[24px] shadow-[0_30px_60px_0px_rgba(61,108,236,0.15)]">
+      <h1 className="desktop:text-[24px] desktop:mb-[36px] mb-[24px] text-[18px] font-bold leading-normal text-[#CAC9FF]">
         Your Result
       </h1>
-      <div className="desktop:h-[200px] desktop:w-[200px] desktop:mb-7 mb-6 flex h-[140px] w-[140px] flex-col items-center justify-center rounded-full bg-gradient-to-b from-[#4D21C9]">
+      <div className="desktop:h-[200px] desktop:w-[200px] desktop:mb-[28px] mb-[24px] flex h-[140px] w-[140px] flex-col items-center justify-center rounded-full bg-gradient-to-b from-[#4D21C9]">
         <p className="desktop:text-[72px] text-[56px] font-extrabold leading-[72px] text-white">76</p>
         <p className="desktop:text-[18px] text-[16px] font-bold leading-normal text-[#CAC9FF] opacity-[.5168]">
           of 100
         </p>
       </div>
       <div className="mx-auto w-[260px] text-center leading-normal">
-        <p className="desktop:text-[32px] desktop:mb-3.5 mb-2 text-[24px] font-bold text-white">Great</p>
+        <p className="desktop:text-[32px] desktop:mb-[14px] mb-[8px] text-[24px] font-bold text-white">Great</p>
         <p className="desktop:text-[18px] text-[16px] font-medium text-[#CAC9FF]">
           Your performance exceed 65% of the people conducting the test here!
         </p>
--- src/components/ResultsSummary/SummarySection.tsx
+++ src/components/ResultsSummary/SummarySection.tsx
@@ -1,66 +1,66 @@
 function SummarySection() {
   return (
-    <section className="desktop:w-1/2 desktop:px-10 desktop:pb-11.5 desktop:pt-9.5 px-7.5 pb-7.5 pt-6">
-      <h2 className="desktop:text-[24px] desktop:mb-7 mb-6 text-[18px] font-bold leading-normal text-[#303B59]">
+    <section className="desktop:w-1/2 desktop:px-[40px] desktop:pb-[46px] desktop:pt-[38px] px-[30px] pb-[30px] pt-[24px]">
+      <h2 className="desktop:text-[24px] desktop:mb-[28px] mb-[24px] text-[18px] font-bold leading-normal text-[#303B59]">
         Summary
       </h2>
-      <ul className="mb-6 space-y-4">
+      <ul className="mb-[24px] space-y-[16px]">
         <li
-          className="flex justify-between rounded-xl px-4 py-4.5"
+          className="flex justify-between rounded-[12px] px-[16px] py-[18px]"
           style={{
             background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%), #F55',
           }}
         >
-          <div className="flex space-x-3">
+          <div className="flex space-x-[12px]">
             <img className="h-[20px]" src="/src/assets/images/icon-reaction.svg" alt="Reaction" />
             <h3 className="desktop:text-[20px] text-[16px] font-medium leading-normal text-[#F55]">Reaction</h3>
           </div>
-          <div className="flex space-x-2"></div>
+          <div className="flex space-x-[8px]"></div>
           <p className="desktop:text-[18px] text-[16px] font-bold leading-normal text-[#303B59]">
             80 <span className="opacity-50">/ 100</span>
           </p>
         </li>
         <li
-          className="flex justify-between rounded-xl px-4 py-4.5"
+          className="flex justify-between rounded-[12px] px-[16px] py-[18px]"
           style={{
             background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%), #FFB21E',
           }}
         >
-          <div className="flex space-x-3">
+          <div className="flex space-x-[12px]">
             <img className="h-[20px]" src="/src/assets/images/icon-memory.svg" alt="Memory" />
             <h3 className="desktop:text-[18px] text-[16px] font-medium leading-normal text-[#FFB21E]">Memory</h3>
           </div>
-          <div className="flex space-x-2"></div>
+          <div className="flex space-x-[8px]"></div>
           <p className="desktop:text-[18px] text-[16px] font-bold leading-normal text-[#303B59]">
             92 <span className="opacity-50">/ 100</span>
           </p>
         </li>
         <li
-          className="flex justify-between rounded-xl px-4 py-4.5"
+          className="flex justify-between rounded-[12px] px-[16px] py-[18px]"
           style={{
             background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%), #00BB8F',
           }}
         >
-          <div className="flex space-x-3">
+          <div className="flex space-x-[12px]">
             <img className="h-[20px]" src="/src/assets/images/icon-verbal.svg" alt="Verbal" />
             <h3 className="desktop:text-[18px] text-[16px] font-medium leading-normal text-[#00BB8F]">Verbal</h3>
           </div>
-          <div className="flex space-x-2"></div>
+          <div className="flex space-x-[8px]"></div>
           <p className="desktop:text-[18px] text-[16px] font-bold leading-normal text-[#303B59]">
             61 <span className="opacity-50">/ 100</span>
           </p>
         </li>
         <li
-          className="flex justify-between rounded-xl px-4 py-4.5"
+          className="flex justify-between rounded-[12px] px-[16px] py-[18px]"
           style={{
             background: 'linear-gradient(0deg, rgba(255, 255, 255, 0.95) 0%, rgba(255, 255, 255, 0.95) 100%), #1125D6',
           }}
         >
-          <div className="flex space-x-3">
+          <div className="flex space-x-[12px]">
             <img className="h-[20px]" src="/src/assets/images/icon-visual.svg" alt="Visual" />
             <h3 className="desktop:text-[18px] text-[16px] font-medium leading-normal text-[#1125D6]">Visual</h3>
           </div>
-          <div className="flex space-x-2"></div>
+          <div className="flex space-x-[8px]"></div>
           <p className="desktop:text-[18px] text-[16px] font-bold leading-normal text-[#303B59]">
             73 <span className="opacity-50">/ 100</span>
           </p>
